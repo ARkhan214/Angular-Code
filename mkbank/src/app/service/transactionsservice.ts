0b15374d547c72fe689adbf3cb714757f7c6a934
MD RAHIM KHAN <abdurrahimkhan214dc@gmail.com>
2025-07-15 19:10:03 +0600

    add

--- mkbank/src/app/service/transactionsservice.ts
+++ mkbank/src/app/service/transactionsservice.ts
@@ -53,6 +53,7 @@ export class Transactionsservice {
           const receiverId = transaction.receiverAccountId;
 
           this.http.get<Accounts>(`${this.accountsUrl}/${receiverId}`).subscribe(receiverAccount => {
+            
             const updatedReceiver = {
               ...receiverAccount,
               balance: receiverAccount.balance + transaction.amount
@@ -71,11 +72,7 @@ export class Transactionsservice {
             console.error('Receiver not found:', error);
           });
         }
-
-
-
-
-
+       
         // Update account balance
         const updatedAccount: Accounts = { ...account, balance: newBalance };
 
@@ -84,8 +81,9 @@ export class Transactionsservice {
             return this.http.post<Transaction>(this.transactionsUrl, transaction);
           })
         );
-      })
+     })
     );
+
   }
 
 
@@ -149,62 +147,7 @@ export class Transactionsservice {
 
 
 
-  //   private baseUrl:string = "http://localhost:3000/transactions"
-
-  //   constructor(
-  //     private http:HttpClient,
-  //      private accountService: Accountsservice   //chat gpt
-  //   ) { }
-
-  //   logTransaction(txn: Transaction): Observable<Transaction> {
-  //     return this.http.post<Transaction>(this.baseUrl, txn);
-  //   }
-  //    saveTransaction(transaction: Transaction): Observable<Transaction[]> {
-  //     return this.http.post<Transaction[]>(this.baseUrl, transaction);
-  //   }
-
-  // //   saveTransaction(transaction: Transaction): Observable<Transaction> {
-  // //   const { id, ...rest } = transaction; // remove id if exists
-  // //   return this.http.post<Transaction>(this.baseUrl, rest);
-  // // }
-
-
-  //   getAllTransactions(): Observable<Transaction[]> {
-  //     return this.http.get<Transaction[]>(this.baseUrl);
-  //   }
-
-  //   getTransactionById(id: string): Observable<Transaction> {
-  //     return this.http.get<Transaction>(`${this.baseUrl}/${id}`);
-  //   }
-
-
-  //   deleteTransaction(id: string): Observable<any> {
-  //   return this.http.delete(`${this.baseUrl}/${id}`);
-  // }
-
-
-
-  // limon bhi er code modifi
-
-  // private baseTransferUrl = 'http://localhost:3000/fundTransfers';
-  // private baseAccountUrl = 'http://localhost:3000/accounts';
-
-  // constructor(private http: HttpClient) {}
-
-  // submitFundTransfer(transferData: any): Observable<any> {
-  //   return this.http.post(this.baseTransferUrl, transferData);
-  // }
-
-  // getAccountByNumber(accountNumber: string): Observable<any> {
-  //   return this.http.get<any[]>(this.baseAccountUrl).pipe(
-  //     map(accounts => accounts.find(acc => acc.accountNumber === accountNumber))
-  //   );
-  // }
-
-  // downloadTransferReceipt(): Observable<Blob> {
-  //   const fakePdfUrl = 'assets/sample-receipt.pdf';
-  //   return this.http.get(fakePdfUrl, { responseType: 'blob' });
-  // }
+  
 
 
 }
